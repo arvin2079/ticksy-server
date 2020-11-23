45709c79780c341b709acd6d4c309012de011c07
arvin2079 <arving2079@gmail.com>
2020-11-23 22:48:55 +0330

    user model created___got error ((admin.E033) The value of 'ordering[0]' refers to 'username', which is not an attribute of 'users.User'.)

--- users/models.py
+++ users/models.py
@@ -1,6 +1,41 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
 
 
-class User(AbstractUser):
-    pass
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('user must have email')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+## TODO : ask if i should add "auto_now_add=True" to "last_login" ???
+
+class User(PermissionsMixin, AbstractBaseUser):
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    email = models.EmailField(unique=True, max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+    last_login = models.DateTimeField(_('last login'), auto_now_add=True)
+    avatar = models.ImageField(_('avatar'), upload_to='avatars/', blank=True, null=True)
+    code = models.IntegerField(_('code'))
+    # role = models.ForeignKey(verbose_name=_('role'))
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
