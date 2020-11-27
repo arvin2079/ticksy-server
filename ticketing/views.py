1b63e03538ef578c8ae2c8bba507b7ec8557e738
Maktoobgar <mahmoodh1378@gmail.com>
2020-11-27 20:52:58 +0330

    adding functionality to topics address

--- ticketing/views.py
+++ ticketing/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from .models import Topic
+from .serializers import TopicSerializer
+from .permissions import Identified
 
-# Create your views here.
+
+class TopicList(generics.ListCreateAPIView):
+    serializer_class = TopicSerializer
+    permission_classes = [IsAuthenticated, Identified]
+
+    def get_queryset(self):
+        return Topic.objects.filter(creator=self.request.user)
