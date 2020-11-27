905144f583f106dbd448a2cc22bac54bf0c27f04
Maktoobgar <mahmoodh1378@gmail.com>
2020-11-27 20:51:56 +0330

    solving model problems

--- ticketing/models.py
+++ ticketing/models.py
@@ -3,10 +3,10 @@ from users.models import User
 
 
 class Topic(models.Model):
-    creator     = models.ForeignKey(User, related_name='created_topics' , on_delete=models.CASCADE, verbose_name='سازنده')
-    title       = models.CharField(max_length=100, blank=True, null=False, verbose_name='موضوع')
-    description = models.CharField(max_length=500, blank=True, null=False, verbose_name='توضیحات')    
-    slug        = models.SlugField(max_length=50, unique=True, blank=False, null=False, verbose_name='تگ آدرس')
+    creator     = models.ForeignKey(User, related_name='created_topics', blank=True, null=True, on_delete=models.CASCADE, verbose_name='سازنده')
+    title       = models.CharField(max_length=100, verbose_name='موضوع')
+    description = models.CharField(max_length=500, verbose_name='توضیحات')
+    slug        = models.SlugField(max_length=50, unique=True, verbose_name='تگ آدرس')
     supporters  = models.ManyToManyField(User, blank=True, related_name='supported_topics', verbose_name='حمایت کنندگان')
 
     def __str__(self):
@@ -37,11 +37,11 @@ class Ticket(models.Model):
     ]
     
     creator         = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='آی دی سازنده تیکت')
-    title           = models.CharField(max_length=100, blank=True, null=False, verbose_name='موضوع')
-    creation_date   = models.DateTimeField(auto_now_add=True, auto_now=False, blank=True, null=False, verbose_name='زمان ایجاد')
-    status          = models.IntegerField(choices=STATUS_CHOICES, default=WAITING_FOR_ANSWER, blank=False, null=False, verbose_name='وضعیت')
-    last_update     = models.DateTimeField(auto_now=True, blank=True, null=False, verbose_name='زمان آخرین تغییرات')
-    priority        = models.IntegerField(choices=PRIORITY_CHOICES, blank=False, null=False, verbose_name='اولویت')
+    title           = models.CharField(max_length=100, verbose_name='موضوع')
+    creation_date   = models.DateTimeField(auto_now_add=True, blank=True, null=True, verbose_name='زمان ایجاد')
+    status          = models.IntegerField(choices=STATUS_CHOICES, default=WAITING_FOR_ANSWER, verbose_name='وضعیت')
+    last_update     = models.DateTimeField(auto_now=True, blank=True, null=True, verbose_name='زمان آخرین تغییرات')
+    priority        = models.IntegerField(choices=PRIORITY_CHOICES, verbose_name='اولویت')
     topic           = models.ForeignKey(Topic, on_delete=models.CASCADE, verbose_name='اطلاعات تیکت')
 
     def __str__(self):
@@ -54,7 +54,7 @@ class Message(models.Model):
     user    = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='سازنده')
     ticket  = models.ForeignKey(Ticket, on_delete=models.CASCADE, verbose_name='اطلاعات تیکت')
     rate    = models.FloatField(blank=False, null=False, default=1, verbose_name='امتیاز')
-    date    = models.DateTimeField(auto_now=False, auto_now_add=True, null=False, blank=True, verbose_name='زمان ایجاد')
+    date    = models.DateTimeField(auto_now=False, auto_now_add=True, null=True, blank=True, verbose_name='زمان ایجاد')
     text    = models.TextField(blank=True, null=False, verbose_name='متن تیکت')
 
     def __str__(self):
@@ -65,7 +65,7 @@ class Message(models.Model):
 
 class Attachment(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE, verbose_name='پیام')
-    file    = models.FileField(upload_to='files/', blank=True, null=True, verbose_name='فایل')
+    file    = models.FileField(upload_to='files/', blank=False, null=False, verbose_name='فایل')
 
     class Meta:
         ordering= ['id']
--- users/models.py
+++ users/models.py
@@ -16,9 +16,9 @@ class Identity(models.Model):
     ]
     user                = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='کاربر')
     identifier_image    = models.ImageField(upload_to='pics/', blank=True, null=True, verbose_name='عکس پرسنلی')
-    request_time        = models.DateTimeField(auto_now=False, auto_now_add=True, blank=True, verbose_name='زمان درخواست')
-    expire_time         = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True, null=True, verbose_name='زمان ابطال')
-    status              = models.IntegerField(choices=STATUS_CHOICES, default=REQUESTED, blank=True, verbose_name='وضعیت')
+    request_time        = models.DateTimeField(auto_now_add=True, blank=True, verbose_name='زمان درخواست')
+    expire_time         = models.DateTimeField(blank=True, verbose_name='زمان ابطال')
+    status              = models.IntegerField(choices=STATUS_CHOICES, default=REQUESTED, verbose_name='وضعیت')
 
     class meta:
         ordering=['user']
