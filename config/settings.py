99d31062ad2fa81f496287967288744cba3524c4
Maktoobgar <mahmoodh1378@gmail.com>
2020-12-25 23:55:46 +0330

    DEFAULT_PAGINATION_CLASS and PAGE_SIZE added to REST_FRAMEWORK

--- config/settings.py
+++ config/settings.py
@@ -104,8 +104,8 @@ REST_FRAMEWORK = {
 
     'DEFAULT_RENDERER_CLASSES': RENDERER,
 
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    #                             'PAGE_SIZE': market_default_variables.DEFAULT_PAGE_SIZE,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 
     # 'DEFAULT_FILTER_BACKENDS': [
     #     'django_filters.rest_framework.DjangoFilterBackend',
