7bc4d47f9d67620e22c8928fb1d19d4ce65e1a0c
Maktoobgar <mahmoodh1378@gmail.com>
2020-12-25 23:59:03 +0330

    custom message with 403 status code will be sent if not identified

--- ticketing/permissions.py
+++ ticketing/permissions.py
@@ -1,31 +1,15 @@
-import datetime
-
+from datetime import datetime
 from rest_framework import permissions
 from users.models import IDENTIFIED
+from rest_framework import status
 
 
 class IsIdentified(permissions.BasePermission):
+    message = 'هویت شما هنوز توسط ادمین تایید نشده است'
+    status_code = status.HTTP_403_FORBIDDEN
 
     def has_permission(self, request, view):
-        # try:
-        #     print(request.user.username)
-        #     user = User.objects.get(username=request.user.username)
-        # except Exception as e:
-        #     print(e)
-        #     return False
-        # if request.user.is_superuser:
-        #     return True
-        # if not request.user == user:
-        #     print(1)
-        #     return False
-        # else:
-        #     return True
         user = request.user
-
-        # todo: remove button section after adding identity signal
-        # -----------------------------
         if not hasattr(user, 'identity'):
             return False
-        # -----------------------------
-
-        return (user.identity.status == IDENTIFIED and (user.identity.expire_time > datetime.datetime.now() if user.identity.expire_time else True)) or user.is_superuser
+        return (user.identity.status == IDENTIFIED and (user.identity.expire_time > datetime.now() if user.identity.expire_time else True)) or user.is_superuser
