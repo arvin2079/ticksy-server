8360100b024dfd92a68c8a0623b343dbbd11e5d5
Maktoobgar <mahmoodh1378@gmail.com>
2020-12-26 00:02:19 +0330

    get_queryset() and pagination_class configed

--- ticketing/views.py
+++ ticketing/views.py
@@ -2,6 +2,7 @@ from django.db.models import Q
 from rest_framework import permissions
 from rest_framework import generics
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
 from .models import Topic
 from .serializers import TopicSerializer
 from .permissions import IsIdentified
@@ -10,6 +11,7 @@ from .permissions import IsIdentified
 class TopicListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = TopicSerializer
     permission_classes = [IsAuthenticated, IsIdentified]
+    pagination_class = PageNumberPagination
 
     def get_queryset(self):
-        return Topic.objects.filter(Q(creator=self.request.user) | Q(supporters__in=[self.request.user])).distinct()
+        return Topic.objects.filter((Q(creator=self.request.user) | Q(supporters__in=[self.request.user])) & Q(is_active='1')).distinct()
