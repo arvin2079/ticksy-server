1b304569e89b3e41f39bdbf3d7d8610f8241274f
Maktoobgar <mahmoodh1378@gmail.com>
2020-12-30 18:00:46 +0330

    adding UserSerializerRestricted class just to show some restricted data from a user to other users

--- users/serializers.py
+++ users/serializers.py
@@ -7,3 +7,15 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model  = User
         fields = ['id', 'username', 'first_name', 'last_name', 'email']
+
+
+class UserSerializerRestricted(serializers.ModelSerializer):
+
+    class Meta:
+        model  = User
+        fields = ['id', 'first_name', 'last_name', 'email']
+        extra_kwargs = {
+            'id': {'read_only': True},
+            'first_name': {'read_only': True},
+            'last_name': {'read_only': True}
+        }
