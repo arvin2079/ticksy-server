d58ec1b43327c6ae28df516d5bfaea60868e69c2
Maktoobgar <mahmoodh1378@gmail.com>
2020-12-30 17:57:36 +0330

    adding IsOwner permission

--- ticketing/permissions.py
+++ ticketing/permissions.py
@@ -2,10 +2,11 @@ from datetime import datetime
 from rest_framework import permissions
 from users.models import IDENTIFIED
 from rest_framework import status
+from rest_framework.permissions import SAFE_METHODS
 
 
 class IsIdentified(permissions.BasePermission):
-    message = 'هویت شما هنوز توسط ادمین تایید نشده است'
+    message = 'هویت شما هنوز توسط ادمین تایید نشده است.'
     status_code = status.HTTP_403_FORBIDDEN
 
     def has_permission(self, request, view):
@@ -13,3 +14,15 @@ class IsIdentified(permissions.BasePermission):
         if not hasattr(user, 'identity'):
             return False
         return (user.identity.status == IDENTIFIED and (user.identity.expire_time > datetime.now() if user.identity.expire_time else True)) or user.is_superuser
+
+
+class IsOwner(permissions.BasePermission):
+    message = 'شما سازنده این تاپیک نیستید.'
+    status_code = status.HTTP_403_FORBIDDEN
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        if obj.creator == request.user:
+            return True
+        return False
