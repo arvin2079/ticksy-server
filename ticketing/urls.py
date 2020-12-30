5ccaa51fb7b61f802fc703fc7a3bd576fd600504
Maktoobgar <mahmoodh1378@gmail.com>
2020-12-30 17:59:07 +0330

    adding /topics/<slug:slug> url

--- ticketing/urls.py
+++ ticketing/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TopicListCreateAPIView
+from .views import TopicListCreateAPIView, TopicRetrieveUpdateDestroyAPIView
 
 
 urlpatterns = [
-    path('topics/', TopicListCreateAPIView.as_view(), name='topic-list-create')
+    path('topics/', TopicListCreateAPIView.as_view(), name='topic-list-create'),
+    path('topics/<slug:slug>', TopicRetrieveUpdateDestroyAPIView.as_view(), name='topic-retrieve-update-destroy')
 ]
