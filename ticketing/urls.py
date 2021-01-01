1796dc79c421377a96fbe28d3ad05d059c5cf485
Maktoobgar <mahmoodh1378@gmail.com>
2021-01-01 14:00:41 +0330

    adding email/ url to search the users by their email

--- ticketing/urls.py
+++ ticketing/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TopicListCreateAPIView, TopicRetrieveUpdateDestroyAPIView
+from .views import TopicListCreateAPIView, TopicRetrieveUpdateDestroyAPIView, EmailListAPIView
 
 
 urlpatterns = [
     path('topics/', TopicListCreateAPIView.as_view(), name='topic-list-create'),
-    path('topics/<slug:slug>', TopicRetrieveUpdateDestroyAPIView.as_view(), name='topic-retrieve-update-destroy')
+    path('topics/<slug:slug>', TopicRetrieveUpdateDestroyAPIView.as_view(), name='topic-retrieve-update-destroy'),
+    path('email/', EmailListAPIView.as_view(), name='email-list')
 ]
