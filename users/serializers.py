36797f555f1793e4167ae3aa32ecc4ef64540fe6
Maktoobgar <mahmoodh1378@gmail.com>
2021-01-01 14:02:37 +0330

    adding a comment

--- users/serializers.py
+++ users/serializers.py
@@ -17,5 +17,13 @@ class UserSerializerRestricted(serializers.ModelSerializer):
         extra_kwargs = {
             'id': {'read_only': True},
             'first_name': {'read_only': True},
-            'last_name': {'read_only': True}
+            'last_name': {'read_only': True},
+            'email': {'read_only': True}
         }
+
+    #def validate(self, attrs):
+        #print("Fucking HERE")
+        #return attrs
+    # hello. if you see this, validate and validate_identity is not actually working.
+    # I actually tried to fix it and identify the users that i pass but validate and
+    # validate_identity just don't work actually.
