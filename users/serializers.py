829f80c34d8727fd361a1a541dbe7c2b6238b0cc
Maktoobgar <mahmoodh1378@gmail.com>
2021-01-03 21:35:24 +0330

    clean up

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,8 @@
+from django.db.models import Q
+from django.utils import timezone
+from datetime import timedelta
 from rest_framework import serializers
-from .models import User
+from .models import User, Identity, IDENTIFIED
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -8,22 +11,9 @@ class UserSerializer(serializers.ModelSerializer):
         model  = User
         fields = ['id', 'username', 'first_name', 'last_name', 'email']
 
-
 class UserSerializerRestricted(serializers.ModelSerializer):
 
     class Meta:
         model  = User
         fields = ['id', 'first_name', 'last_name', 'email']
-        extra_kwargs = {
-            'id': {'read_only': True},
-            'first_name': {'read_only': True},
-            'last_name': {'read_only': True},
-            'email': {'read_only': True}
-        }
-
-    #def validate(self, attrs):
-        #print("Fucking HERE")
-        #return attrs
-    # hello. if you see this, validate and validate_identity is not actually working.
-    # I actually tried to fix it and identify the users that i pass but validate and
-    # validate_identity just don't work actually.
+        read_only_fields = ['id', 'first_name', 'last_name', 'email']
