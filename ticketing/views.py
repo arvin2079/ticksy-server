9a44a3a82ccff8269141051a10a4320fb60de1e7
Maktoobgar <mahmoodh1378@gmail.com>
2021-01-10 15:20:00 +0330

    adding new functionalities for ticket and message and email

--- ticketing/views.py
+++ ticketing/views.py
@@ -1,6 +1,7 @@
 from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import permissions, status, generics, filters
 from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
@@ -9,10 +10,11 @@ from users.models import IDENTIFIED
 from users.serializers import UserSerializerRestricted
 from users.models import User
 from datetime import timedelta
-from .models import Topic, DEACTIVE
-from .serializers import TopicsSerializer, TopicSerializer
-from .permissions import IsIdentified, IsOwner
+from .models import Topic, Ticket, Topic, Message, DEACTIVE
+from .serializers import TopicsSerializer, TopicSerializer, TicketSerializer, MessageSerializer, MessageUpdateSerializer
+from .permissions import IsIdentified, IsOwner, IsTicketOwnerOrTopicOwner, IsSupporterOrOwnerOrTicketCreator
 from .swagger import get_email_dictionary_response, delete_topic_dictionary_response, get_topic_dictionary_response, get_topics_dictionary_response, post_topics_dictionary_response, put_topic_dictionary_response, put_topic_dictionary_request_body, post_topic_dictionary_request_body
+from .filters import TicketFilter
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 
@@ -21,6 +23,7 @@ class TopicListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = TopicsSerializer
     permission_classes = [IsAuthenticated, IsIdentified]
     pagination_class = PageNumberPagination
+    http_method_names = ['get', 'post']
 
     @swagger_auto_schema(operation_description='Returns a list full of Topics that a user is supporter or creator of them.\nmethod: GET\nurl: /topics', responses=get_topics_dictionary_response)
     def get(self, request, *args, **kwargs):
@@ -31,13 +34,14 @@ class TopicListCreateAPIView(generics.ListCreateAPIView):
         return self.create(request, *args, **kwargs)
 
     def get_queryset(self):
-        return Topic.objects.filter((Q(creator=self.request.user) | Q(supporters__in=[self.request.user])) & Q(is_active='1')).distinct()
+        return Topic.objects.filter((Q(creator=self.request.user) | Q(supporters__in=[self.request.user])) & Q(is_active='1')).distinct().order_by('-id')
+
 
 class TopicRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TopicSerializer
     permission_classes = [IsAuthenticated, IsIdentified, IsOwner]
     lookup_field = 'slug'
-    allowed_methods = ['GET', 'PUT', 'DELETE']
+    http_method_names = ['get', 'put', 'delete']
 
     @swagger_auto_schema(operation_description='Searches for a topic and returns the topic that has a slug exactly matched with the url slug(if exist).\nmethod: GET\nurl: /topics/\{slug\}\nexample: /topics/amoozesh-khu', responses=get_topic_dictionary_response)
     def get(self, request, *args, **kwargs):
@@ -63,12 +67,14 @@ class TopicRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
         instance.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+
 class EmailListAPIView(generics.ListAPIView):
     serializer_class = UserSerializerRestricted
     permission_classes = [IsAuthenticated, IsIdentified]
     search_fields = ['email']
     filter_backends = [filters.SearchFilter]
     pagination_class = None
+    http_method_names = ['get']
 
     @swagger_auto_schema(operation_description='Just searchs for 10 first emails containing the searched text.\nmethod: GET\nurl: /email/?search=\{Text\}\nexample: /email/?search=karami', responses=get_email_dictionary_response, query_serializer=UserSerializerRestricted)
     def get(self, request, *args, **kwargs):
@@ -84,3 +90,37 @@ class EmailListAPIView(generics.ListAPIView):
         queryset = queryset[:10]
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
+
+
+class TicketListCreateAPIView(generics.ListCreateAPIView):
+    serializer_class = TicketSerializer
+    permission_classes = [IsAuthenticated, IsIdentified]
+    search_fields = ['id', 'title']
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    filterset_class = TicketFilter
+    pagination_class = None
+    http_method_names = ['get', 'post']
+
+    def get_queryset(self):
+        return Ticket.objects.filter(Q(topic=Topic.objects.get(slug=self.kwargs.get('slug')))).order_by('-id')
+
+
+class MessageListCreateAPIView(generics.ListCreateAPIView):
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated, IsIdentified, IsTicketOwnerOrTopicOwner]
+    pagination_class = None
+    http_method_names = ['get', 'post']
+
+    def get_queryset(self):
+        return Message.objects.filter(Q(ticket=self.kwargs.get('id'))).distinct().order_by('id')
+
+class MessageUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = MessageUpdateSerializer
+    permission_classes = [IsAuthenticated, IsIdentified, IsSupporterOrOwnerOrTicketCreator]
+    http_method_names = ['patch']
+
+    def get_queryset(self):
+        return Message.objects.filter(id=self.kwargs.get('messageid'))
+    
+    def get_object(self):
+        return get_object_or_404(self.get_queryset())
