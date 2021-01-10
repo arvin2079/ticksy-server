f1f1802ac9afacaab01a0835fba8f76e9a5ddefb
Maktoobgar <mahmoodh1378@gmail.com>
2021-01-10 15:19:32 +0330

    adding new urls for ticket and message and email

--- ticketing/urls.py
+++ ticketing/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import TopicListCreateAPIView, TopicRetrieveUpdateDestroyAPIView, EmailListAPIView
+from .views import TopicListCreateAPIView, TopicRetrieveUpdateDestroyAPIView, EmailListAPIView, TicketListCreateAPIView, MessageListCreateAPIView, MessageUpdateAPIView
 
 
 urlpatterns = [
     path('topics/', TopicListCreateAPIView.as_view(), name='topic-list-create'),
-    path('topics/<slug:slug>', TopicRetrieveUpdateDestroyAPIView.as_view(), name='topic-retrieve-update-destroy'),
+    path('topics/<slug:slug>/', TopicRetrieveUpdateDestroyAPIView.as_view(), name='topic-retrieve-update-destroy'),
+    path('topics/<slug:slug>/tickets/', TicketListCreateAPIView.as_view(), name='ticket-list-create'),
+    path('topics/<slug:slug>/tickets/<int:id>/', MessageListCreateAPIView.as_view(), name='message-list-create'),
+    path('topics/<slug:slug>/tickets/<int:id>/message/<int:messageid>/', MessageUpdateAPIView.as_view(), name='message-rateupdate'),
     path('email/', EmailListAPIView.as_view(), name='email-list')
 ]
