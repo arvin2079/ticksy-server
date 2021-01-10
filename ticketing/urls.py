df9696d0af6d7869a1b38d3d2e78b8afdd7fd2c0
Maktoobgar <mahmoodh1378@gmail.com>
2021-01-10 21:17:26 +0330

    edited message url

--- ticketing/urls.py
+++ ticketing/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('topics/<slug:slug>/', TopicRetrieveUpdateDestroyAPIView.as_view(), name='topic-retrieve-update-destroy'),
     path('topics/<slug:slug>/tickets/', TicketListCreateAPIView.as_view(), name='ticket-list-create'),
     path('topics/<slug:slug>/tickets/<int:id>/', MessageListCreateAPIView.as_view(), name='message-list-create'),
-    path('topics/<slug:slug>/tickets/<int:id>/message/<int:messageid>/', MessageUpdateAPIView.as_view(), name='message-rateupdate'),
+    path('message/<int:id>/', MessageUpdateAPIView.as_view(), name='message-rate-update'),
     path('email/', EmailListAPIView.as_view(), name='email-list')
 ]
