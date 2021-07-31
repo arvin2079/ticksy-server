bcd5b70ac6607cbe6e02ce1de0138bd14c84c5c2
AmirAli Sabouri <amiralifmj@gmail.com>
2021-07-31 23:54:21 +0430

    bug fix

--- ticketing/permissions.py
+++ ticketing/permissions.py
@@ -27,12 +27,15 @@ class IsOwner(permissions.BasePermission):
         return request.method in permissions.SAFE_METHODS or request.user == obj.creator
 
 
-class IsTopicOwner(permissions.BasePermission):
+class IsTopicOwnerOrSupporter(permissions.BasePermission):
     message = 'شما سازنده این تاپیک نیستید.'
     status_code = status.HTTP_403_FORBIDDEN
 
     def has_permission(self, request, view):
-        return request.method in permissions.SAFE_METHODS or request.user == get_object_or_404(Topic, id=view.kwargs.get('id')).creator
+        topic = get_object_or_404(Topic, id=view.kwargs.get('id'))
+        if request.method in permissions.SAFE_METHODS:
+            return request.user == topic.creator or topic.admins.filter(users__in=[request.user.id]).exists()
+        return request.user == topic.creator
 
 
 class HasAccessToRoll(permissions.BasePermission):
